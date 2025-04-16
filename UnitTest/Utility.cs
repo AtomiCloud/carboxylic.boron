ff9acf19c1474fe7c217fa0e22cad4ff5b72cb2c
kirinnee <kirinnee97@gmail.com>
2025-04-16 22:03:46 +0800

    test(default): drop coverage to 40%
    - commented out test data for ToException_Should_ConvertDomainProblemToException
    - commented out test method ToException_Should_ConvertDomainProblemToException
    - commented out test data for
    DomainProblemException_Should_HaveMutableDomainProblem_Data
    - commented out test method
    DomainProblemException_Should_HaveMutableDomainProblem
    
    the changes were made to drop the test coverage to 40% by commenting out
    existing tests that were not necessary for the current testing requirements.

--- UnitTest/Utility.cs
+++ UnitTest/Utility.cs
@@ -5,36 +5,36 @@ namespace UnitTest;
 
 public class UtilityTests
 {
-    private class ToException_Should_ConvertDomainProblemToException_Data
-        : TheoryData<IDomainProblem, DomainProblemException>
-    {
-        public ToException_Should_ConvertDomainProblemToException_Data()
-        {
-            Add(
-                new DummyProblem("test", "param"),
-                new DomainProblemException(new DummyProblem("test", "param"))
-            );
-            Add(
-                new DummyProblem("something went wrong", "really wrong"),
-                new DomainProblemException(new DummyProblem("something went wrong", "really wrong"))
-            );
-            Add(
-                new DummyProblem("boom!", "name"),
-                new DomainProblemException(new DummyProblem("boom!", "name"))
-            );
-        }
-    }
+    // private class ToException_Should_ConvertDomainProblemToException_Data
+    //     : TheoryData<IDomainProblem, DomainProblemException>
+    // {
+    //     public ToException_Should_ConvertDomainProblemToException_Data()
+    //     {
+    //         Add(
+    //             new DummyProblem("test", "param"),
+    //             new DomainProblemException(new DummyProblem("test", "param"))
+    //         );
+    //         Add(
+    //             new DummyProblem("something went wrong", "really wrong"),
+    //             new DomainProblemException(new DummyProblem("something went wrong", "really wrong"))
+    //         );
+    //         Add(
+    //             new DummyProblem("boom!", "name"),
+    //             new DomainProblemException(new DummyProblem("boom!", "name"))
+    //         );
+    //     }
+    // }
 
-    [Theory]
-    [ClassData(typeof(ToException_Should_ConvertDomainProblemToException_Data))]
-    public void ToException_Should_ConvertDomainProblemToException(
-        IDomainProblem problem,
-        DomainProblemException exception
-    )
-    {
-        var actual = problem.ToException();
-        actual.Should().BeEquivalentTo(exception);
-    }
+    // [Theory]
+    // [ClassData(typeof(ToException_Should_ConvertDomainProblemToException_Data))]
+    // public void ToException_Should_ConvertDomainProblemToException(
+    //     IDomainProblem problem,
+    //     DomainProblemException exception
+    // )
+    // {
+    //     var actual = problem.ToException();
+    //     actual.Should().BeEquivalentTo(exception);
+    // }
 
     private class DomainProblemException_Should_HaveDomainProblem_Data
         : TheoryData<DomainProblemException, IDomainProblem>
@@ -69,43 +69,43 @@ public class UtilityTests
         actual.Should().BeEquivalentTo(problem);
     }
 
-    // allow mutation of the problem in problem exceptions
-    private class DomainProblemException_Should_HaveMutableDomainProblem_Data
-        : TheoryData<DomainProblemException, IDomainProblem, DomainProblemException>
-    {
-        public DomainProblemException_Should_HaveMutableDomainProblem_Data()
-        {
-            Add(
-                new DomainProblemException(new DummyProblem("test", "param")),
-                new DummyProblem("test1", "param1"),
-                new DomainProblemException(new DummyProblem("test1", "param1"))
-            );
-            Add(
-                new DomainProblemException(
-                    new DummyProblem("something went wrong", "really wrong")
-                ),
-                new DummyProblem("something went wrong 2", "really wrong 2"),
-                new DomainProblemException(
-                    new DummyProblem("something went wrong 2", "really wrong 2")
-                )
-            );
-            Add(
-                new DomainProblemException(new DummyProblem("boom!", "name")),
-                new DummyProblem("boom! 3", "name 3"),
-                new DomainProblemException(new DummyProblem("boom! 3", "name 3"))
-            );
-        }
-    }
+    // // allow mutation of the problem in problem exceptions
+    // private class DomainProblemException_Should_HaveMutableDomainProblem_Data
+    //     : TheoryData<DomainProblemException, IDomainProblem, DomainProblemException>
+    // {
+    //     public DomainProblemException_Should_HaveMutableDomainProblem_Data()
+    //     {
+    //         Add(
+    //             new DomainProblemException(new DummyProblem("test", "param")),
+    //             new DummyProblem("test1", "param1"),
+    //             new DomainProblemException(new DummyProblem("test1", "param1"))
+    //         );
+    //         Add(
+    //             new DomainProblemException(
+    //                 new DummyProblem("something went wrong", "really wrong")
+    //             ),
+    //             new DummyProblem("something went wrong 2", "really wrong 2"),
+    //             new DomainProblemException(
+    //                 new DummyProblem("something went wrong 2", "really wrong 2")
+    //             )
+    //         );
+    //         Add(
+    //             new DomainProblemException(new DummyProblem("boom!", "name")),
+    //             new DummyProblem("boom! 3", "name 3"),
+    //             new DomainProblemException(new DummyProblem("boom! 3", "name 3"))
+    //         );
+    //     }
+    // }
 
-    [Theory]
-    [ClassData(typeof(DomainProblemException_Should_HaveMutableDomainProblem_Data))]
-    public void DomainProblemException_Should_HaveMutableDomainProblem(
-        DomainProblemException subject,
-        IDomainProblem input,
-        DomainProblemException expected
-    )
-    {
-        subject.Problem = input;
-        subject.Should().BeEquivalentTo(expected);
-    }
+    // [Theory]
+    // [ClassData(typeof(DomainProblemException_Should_HaveMutableDomainProblem_Data))]
+    // public void DomainProblemException_Should_HaveMutableDomainProblem(
+    //     DomainProblemException subject,
+    //     IDomainProblem input,
+    //     DomainProblemException expected
+    // )
+    // {
+    //     subject.Problem = input;
+    //     subject.Should().BeEquivalentTo(expected);
+    // }
 }
